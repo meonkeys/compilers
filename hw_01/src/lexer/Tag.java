e48420df1889599dbbc3e08af5017b94366f98bf
Adam Monsen <haircut@gmail.com>
2009-01-30 20:39:00 -0600

    Import source as provided by professor.
    See:
    http://www-users.itlabs.umn.edu/classes/Spring-2009/csci5161/index.php?page=assignment

---
+++ hw_01/src/lexer/Tag.java
@@ -0,0 +1,25 @@
+package lexer;
+
+public class Tag
+{
+    public static final int AND   = 256;
+    public static final int BASIC = 257;
+    public static final int BREAK = 258;
+    public static final int DO    = 259;
+    public static final int ELSE  = 260;
+    public static final int EQ    = 261;
+    public static final int FALSE = 262;
+    public static final int GE    = 263;
+    public static final int ID    = 264;
+    public static final int IF    = 265;
+    public static final int INDEX = 266;
+    public static final int LE    = 267;
+    public static final int MINUS = 268;
+    public static final int NE    = 269;
+    public static final int NUM   = 270;
+    public static final int OR    = 271;
+    public static final int REAL  = 272;
+    public static final int TEMP  = 273;
+    public static final int TRUE  = 274;
+    public static final int WHILE = 275;
+}
---
+++ hw_01/src/lexer/Token.java
@@ -0,0 +1,16 @@
+package lexer;
+
+public class Token
+{
+    public final int tag;
+
+    public Token(int t)
+    {
+	tag = t;
+    }
+
+    public String toString()
+    {
+	return "" + (char) tag;
+    }
+}
