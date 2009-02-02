25de4001b783a208cd6f143c772c3a970b5a0b71
Joe Myre <myrex013@umn.edu>
2009-02-02 08:35:32 -0600

    Patch from Joe implementing constant folding.
    "I basically included a check in the gen() method of the Arith class
    that implements constant folding. It seemed reasonable to put the
    constant folding in the gen() method from what the appendix said."

--- hw_01/src/inter/Arith.java
+++ hw_01/src/inter/Arith.java
@@ -16,6 +16,71 @@ public class Arith extends Op {
 	}
 
 	public Expr gen() {
+		boolean isOp1int = true;
+		boolean isOp2int = true;
+		boolean isOp1float = true;
+		boolean isOp2float = true;
+
+		try{
+			Integer.parseInt(expr1.toString());
+		}catch(NumberFormatException nfe){
+			isOp1int = false;
+		}
+
+		try{
+			Integer.parseInt(expr2.toString());
+		}catch(NumberFormatException nfe){
+			isOp2int = false;
+		}
+
+		try{
+			Double.parseDouble(expr1.toString());
+		}catch(NumberFormatException nfe){
+			isOp1float = false;
+		}
+
+		try{
+			Double.parseDouble(expr2.toString());
+		}catch(NumberFormatException nfe){
+			isOp2float = false;
+		}
+
+		if(isOp1int && isOp2int){
+			int result = 0;
+			int op1 = Integer.parseInt(expr1.toString());
+			int op2 = Integer.parseInt(expr2.toString());
+			if(op.toString().equals("+")){
+				result = op1 + op2;
+			}
+			else if(op.toString().equals("-")){
+				result = op1 - op2;
+			}
+			else if(op.toString().equals("*")){
+				result = op1 * op2;
+			}
+			else if(op.toString().equals("/")){
+				result = op1 / op2;
+			}
+			return new Constant(new Num(result), Type.Int);
+		}
+		else if(isOp1float && isOp2float){
+			float result = (float)0.0;
+			float op1 = Float.parseFloat(expr1.toString());
+			float op2 = Float.parseFloat(expr2.toString());
+			if(op.toString().equals("+")){
+				result = op1 + op2;
+			}
+			else if(op.toString().equals("-")){
+				result = op1 - op2;
+			}
+			else if(op.toString().equals("*")){
+				result = op1 * op2;
+			}
+			else if(op.toString().equals("/")){
+				result = op1 / op2;
+			}
+			return new Constant(new Real(result), Type.Float);
+		}
 		return new Arith(op, expr1.reduce(), expr2.reduce());
 	}
 
