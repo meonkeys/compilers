94d1f03198f205a537b358008b7912ed2706fdc5
Adam Monsen <haircut@gmail.com>
2009-02-03 20:36:52 -0600

    Added unit test framework.
    Edit Test.tests static array first.
    To execute, I use "java -cp bin main.Test"
    (assuming classes are compiled to a "bin" directory)

--- hw_01/src/main/Main.java
+++ hw_01/src/main/Main.java
@@ -7,14 +7,18 @@ import parser.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStream old_stdin = System.in;
-		/* Assume first arg is file to read from, instead of standard input. */ 
+		/* Assume first arg is file to read from, instead of standard input. */
 		if (args.length > 0) {
 			System.setIn(new FileInputStream(args[0]));
 		}
+		compile();
+		System.setIn(old_stdin);
+	}
+
+	public static void compile() throws IOException {
 		Lexer lex = new Lexer();
 		Parser parse = new Parser(lex);
 		parse.program();
 		System.out.write('\n');
-		System.setIn(old_stdin);
 	}
 }
