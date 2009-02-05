9149cc83bb327271dec87e4dc397e4770bfbb900
Adam Monsen <haircut@gmail.com>
2009-02-04 21:09:51 -0600

    Added unit tests from assignment description.
    Also:
    * distinguish between errors and failures: errors are when compilation
      breaks with a syntax error in the source, failure is when the compiled
      output doesn't match expected intermediate code
    * automatically find any/all unit tests in the "tests" dir
    
    There still seems to be a problem with fold03... it should have the
    label L1 on the first line.

--- hw_01/src/main/Test.java
+++ hw_01/src/main/Test.java
@@ -1,24 +1,33 @@
 package main;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test {
-	static String[] tests = { "array", "fromBook", "precedence", "simple" };
-
 	public static void main(String[] args) throws IOException {
 		InputStream old_stdin = System.in;
 		PrintStream old_stdout = System.out;
-		boolean isError = false;
+		boolean somethingBroke = false;
 
-		for (String s : tests) {
+		for (String s : findTests(args[0])) {
 			// setUp
+			boolean isError = false;
 			System.setIn(new FileInputStream(args[0] + File.separator + s));
 			StringSavePrintStream temp = new StringSavePrintStream();
 			PrintStream pstream = new PrintStream(temp);
 			System.setOut(pstream);
 
 			// run test
-			Main.compile();
+			System.err.print("test: " + s + " ...");
+			try {
+				Main.compile();
+			} catch (Error e) {
+				somethingBroke = true;
+				isError = true;
+				System.err.println("ERROR");
+				e.printStackTrace(System.err);
+			}
 
 			// tearDown
 			temp.flush();
@@ -26,20 +35,43 @@ public class Test {
 			System.setIn(old_stdin);
 			System.setOut(old_stdout);
 
+			if (isError)
+				continue;
+
 			// report
 			String expected = slurp(args[0] + File.separator + s + ".i");
 			if (!expected.equals(actual)) {
-				System.out.println("ERROR in test: " + s);
-				System.out.println("EXPECTED:\n" + expected);
-				System.out.println("BUT WAS:\n" + actual);
-				isError = true;
+				System.out.println("FAILURE");
+				System.out.println("expected:\n" + expected);
+				System.out.println("actual:\n" + actual);
+				somethingBroke = true;
+				continue;
 			}
+
+			System.err.println(" OK");
 		}
 
-		if (isError)
+		if (somethingBroke)
 			System.exit(1);
 		else
 			System.exit(0);
+	}
+
+	private static String[] findTests(String searchDir) {
+		String[] rv;
+		ArrayList tests = new ArrayList();
+		for (File f : new File(searchDir).listFiles()) {
+			if (!f.isFile())
+				continue;
+			if (f.getName().matches("^(\\.|_)"))
+				continue;
+			if (f.getName().matches("^.+\\.i$"))
+				continue;
+			tests.add(f.getName());
+		}
+		rv = (String[]) tests.toArray(new String[0]);
+		Arrays.sort(rv);
+		return rv;
 	}
 
 	private static String slurp(String filePath) throws IOException {
