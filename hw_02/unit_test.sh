d5f384dceaed4046f18ec2dbfb141e993bcf0970
Adam Monsen <haircut@gmail.com>
2009-02-09 02:16:52 -0600

    Line counting and rudimentary recognition.
    * Documented notes, questions, TODOs.
    * Added an example from the lex/yacc book.
    * Added warn and nodefault options for safety (as suggested by flex manual).
    * Adding noinput nounput options eliminates some unused method warnings, which
      means I can now use -Wall -Werror for ultra-strict sanity checking.
    * Start churning out a C-- ruleset.
    * Start working on reporting.
    * Added a unit test using code/output provided in assignment description.

---
+++ hw_02/unit_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+tmpdir=`mktemp -d`
+for t in `\ls tests/*.in`
+do
+    echo "test: $t"
+    testBase=`basename $t .in`
+    outFile=$testBase.out
+    ./scan < $t > $tmpdir/$outFile
+    diff tests/$outFile $tmpdir/$outFile
+done
+rm -rf $tmpdir
+echo "ALL TESTS PASSED"
