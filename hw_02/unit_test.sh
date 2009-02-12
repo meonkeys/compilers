1faf675a7a808f8f016cf090f33d32c9d2d0d46c
joe <joe@THEM.(none)>
2009-02-12 15:04:30 -0600

    Updated unit_test.sh to tee output of lexer to file and stdout.  Removed start_comment() and end_comment() from scanner.  Added output in main().

--- hw_02/unit_test.sh
+++ hw_02/unit_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -o errexit
 tmpdir=`mktemp -d`
+#echo "tmpdir: $tmpdir"
 for t in `\ls tests/*.in`
 do
     echo "test: $t"
     testBase=`basename $t .in`
     outFile=$testBase.out
-    ./scan < $t > $tmpdir/$outFile
+    ./scan < $t | tee $tmpdir/$outFile
     diff tests/$outFile $tmpdir/$outFile
 done
 rm -rf $tmpdir
